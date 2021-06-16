cd81db3b2154f81f9fccacf86105d44075a01e64
Alex Alonso <alex.alonso@ae.studio>
2021-06-15 21:01:58 -0300

    add parameter to video query, implements videos query

--- index.js
+++ index.js
@@ -1,75 +1,78 @@
-'use strict';
+"use strict";
 
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLBoolean, GraphQLID } = require('graphql');
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLList,
+} = require("graphql");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 const PORT = process.env.PORT || 3000;
 const server = express();
+const { getVideoById, getVideos } = require("./src/data");
 
 const videoType = new GraphQLObjectType({
-  name: 'Video',
-  description: 'A video on Egghead.io',
+  name: "Video",
+  description: "A video on Egghead.io",
   fields: {
     id: {
       type: GraphQLID,
-      description: 'The id of the video.',
+      description: "The id of the video.",
     },
     title: {
       type: GraphQLString,
-      description: 'The title of the video.',
+      description: "The title of the video.",
     },
     duration: {
       type: GraphQLInt,
-      description: 'The duration of the video (in seconds)',
+      description: "The duration of the video (in seconds)",
     },
     watched: {
       type: GraphQLBoolean,
-      description: 'Whether or not the viewer has watched the video',
+      description: "Whether or not the viewer has watched the video",
     },
   },
 });
 
 const queryType = new GraphQLObjectType({
-  name: 'Query',
-  description: 'The root query type',
+  name: "Query",
+  description: "The root query type",
   fields: {
+    videos: {
+      type: new GraphQLList(videoType),
+      resolve: getVideos,
+    },
     video: {
       type: videoType,
-      resolve: () =>
-        Promise.resolve({
-          id: 'a',
-          title: 'GraphQL',
-          duration: 180,
-          watched: false,
-        }),
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+          description: "The ID of the video",
+        },
+      },
+      resolve: (_, args) => {
+        return getVideoById(args.id);
+      },
     },
   },
+  videos: {},
 });
 
 const schema = new GraphQLSchema({
   query: queryType,
 });
 
-const videoA = {
-  id: 'a',
-  title: 'Create a GraphQL Schema',
-  duration: 120,
-  watched: true,
-};
-
-const videoB = {
-  id: 'b',
-  title: 'Ember.js CLI',
-  duration: 240,
-  watched: false,
-};
-
 server.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
     schema,
     graphiql: true,
-  }),
+  })
 );
 
 server.listen(PORT, () => {
